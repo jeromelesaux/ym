111fee8cdfa4629e979e7e0d7eed256014b5c141
Jerome Le Saux <jeromelesaux@gmail.com>
2020-04-08 23:36:29 +0200

    first commit

---
+++ encoding/encoder.go
@@ -0,0 +1,161 @@
+package encoding
+
+import (
+	"bytes"
+	"encoding/binary"
+
+	"github.com/jeromelesaux/ym"
+)
+
+func Unmarshall(data []byte, y *ym.Ym) error {
+	r := bytes.NewReader(data)
+	if err := binary.Read(r, binary.BigEndian, y.FileID); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.BigEndian, y.CheckString); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.BigEndian, y.NbFrames); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.BigEndian, y.SongAttributes); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.BigEndian, y.DigidrumNb); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.BigEndian, y.YmMasterClock); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.BigEndian, y.FrameHz); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.BigEndian, y.LoopFrame); err != nil {
+		return err
+	}
+	if err := binary.Read(r, binary.BigEndian, y.Size); err != nil {
+		return err
+	}
+	if y.DigidrumNb > 0 {
+		y.Digidrums = make([]ym.Digidrum, y.DigidrumNb)
+		for i := 0; i < int(y.DigidrumNb); i++ {
+			d := ym.Digidrum{}
+			if err := binary.Read(r, binary.BigEndian, &d); err != nil {
+				return err
+			}
+			y.Digidrums[i] = d
+
+		}
+	}
+	songName := []byte{}
+	for {
+		b, err := r.ReadByte()
+		if err != nil {
+			return err
+		}
+		songName = append(songName, b)
+		if b == 0 {
+			copy(y.SongName, songName)
+		}
+	}
+	authorName := []byte{}
+	for {
+		b, err := r.ReadByte()
+		if err != nil {
+			return err
+		}
+		authorName = append(authorName, b)
+		if b == 0 {
+			copy(y.AuthorName, authorName)
+		}
+	}
+	songComment := []byte{}
+	for {
+		b, err := r.ReadByte()
+		if err != nil {
+			return err
+		}
+		songComment = append(songComment, b)
+		if b == 0 {
+			copy(y.SongComment, songComment)
+		}
+	}
+
+	for i := 0; i <= 15; i++ {
+		y.Data[i] = make([]byte, y.NbFrames)
+	}
+	var err error
+	for i := 0; i < int(y.NbFrames); i++ {
+		for j := 0; j < 16; j++ {
+			y.Data[j][i], err = r.ReadByte()
+			if err != nil {
+				return err
+			}
+		}
+	}
+	if err := binary.Read(r, binary.BigEndian, y.EndID); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Marshall(y *ym.Ym) ([]byte, error) {
+	var b bytes.Buffer
+	if err := binary.Write(&b, binary.BigEndian, y.FileID); err != nil {
+		return b.Bytes(), err
+	}
+	if err := binary.Write(&b, binary.BigEndian, y.CheckString); err != nil {
+		return b.Bytes(), err
+	}
+	if err := binary.Write(&b, binary.BigEndian, y.NbFrames); err != nil {
+		return b.Bytes(), err
+	}
+	if err := binary.Write(&b, binary.BigEndian, y.SongAttributes); err != nil {
+		return b.Bytes(), err
+	}
+	if err := binary.Write(&b, binary.BigEndian, y.DigidrumNb); err != nil {
+		return b.Bytes(), err
+	}
+	if err := binary.Write(&b, binary.BigEndian, y.YmMasterClock); err != nil {
+		return b.Bytes(), err
+	}
+	if err := binary.Write(&b, binary.BigEndian, y.FrameHz); err != nil {
+		return b.Bytes(), err
+	}
+	if err := binary.Write(&b, binary.BigEndian, y.LoopFrame); err != nil {
+		return b.Bytes(), err
+	}
+	if err := binary.Write(&b, binary.BigEndian, y.Size); err != nil {
+		return b.Bytes(), err
+	}
+	if y.DigidrumNb > 0 {
+		for i := 0; i < int(y.DigidrumNb); i++ {
+			if err := binary.Write(&b, binary.BigEndian, y.Digidrums[i]); err != nil {
+				return b.Bytes(), err
+			}
+		}
+	}
+
+	if err := binary.Write(&b, binary.BigEndian, y.SongName); err != nil {
+		return b.Bytes(), err
+	}
+	if err := binary.Write(&b, binary.BigEndian, y.AuthorName); err != nil {
+		return b.Bytes(), err
+	}
+	if err := binary.Write(&b, binary.BigEndian, y.SongComment); err != nil {
+		return b.Bytes(), err
+	}
+	var err error
+	for i := 0; i < int(y.NbFrames); i++ {
+		for j := 0; j < 16; j++ {
+			err = b.WriteByte(y.Data[j][i])
+			if err != nil {
+				return b.Bytes(), err
+			}
+		}
+	}
+	if err := binary.Write(&b, binary.BigEndian, y.EndID); err != nil {
+		return b.Bytes(), err
+	}
+	return b.Bytes(), err
+}
---
+++ ym.go
@@ -0,0 +1,28 @@
+package ym
+
+type Ym struct {
+	FileID         uint32
+	CheckString    [8]byte
+	NbFrames       uint32
+	SongAttributes uint32
+	DigidrumNb     uint16
+	YmMasterClock  uint32
+	FrameHz        uint32
+	LoopFrame      uint32
+	Size           uint16
+	Digidrums      []Digidrum
+	SongName       []byte
+	AuthorName     []byte
+	SongComment    []byte
+	Data           [15][]byte
+	EndID          uint32
+}
+
+type Digidrum struct {
+	SampleSize uint32
+	SampleData []byte
+}
+
+func NewYm() *Ym {
+	return &Ym{}
+}
